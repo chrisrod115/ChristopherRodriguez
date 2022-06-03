c3eef4a98bf53ad2076d3eb38d81a361eaf920cf
Christopher Rodriguez <chrisrod115@gmail.com>
2022-06-02 22:15:30 -0700

    Day one of the 100 days of code
    Finished the two files along with setting up my desktop today.

---
+++ day_1/day_1_final.py
@@ -0,0 +1,20 @@
+"""
+My code --> 
+"""
+
+print("Welcome to the band generator")
+name_of_city = input("what is the name of the city that you grew up in? ")
+pets_name = input("what is your pets name? ")
+
+print("your bands name could be:",name_of_city,pets_name)
+
+"""
+Professor code -->
+"""
+
+"""print("Welcome to the band generator")
+name_of_city = input("what is the name of the city that you grew up in? ")
+pets_name = input("what is your pets name? ")
+
+print("your bands name could be:"+name_of_city+" "+pets_name)"""
+
