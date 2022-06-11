c4e9a32b6a9421f33ddc47f2f22d65f3e7e211b7
Christopher Rodriguez <chrisrod115@gmail.com>
2022-06-10 23:54:35 -0700

    Version 1
    Final version that works. Needs some improvements I feel like it can be simpler!

--- day_004/heads_tail.py
+++ day_004/heads_tail.py
@@ -6,6 +6,7 @@ You are going to write a virtual coin toss program. It will randomly tell the us
 
 Important, the first letter should be capitalised and spelt exactly like in the example e.g. Heads, not heads.
 
+
 There are many ways of doing this. But to practice what we learnt in the last lesson, you should generate a random number, either 0 or 1. Then use that number to print out Heads or Tails.
 
 e.g. 1 means Heads 0 means Tails
@@ -13,10 +14,10 @@ e.g. 1 means Heads 0 means Tails
 playing = True
 
 while playing != False:
-
-    action = int(input("Type \"roll\" to flip a coin, or type \"stop\" to quit"))
-
-    while action == "roll":
+    
+    action = (input("Type \"roll\" to flip a coin, or type \"stop\" to quit: "))
+    
+    if action == "roll":
         heads_or_tails = random.randint(0,1)
         if heads_or_tails == 0:
             print("Heads")
@@ -24,6 +25,10 @@ while playing != False:
         elif heads_or_tails == 1: 
             print("Tails")
             continue
-        else: 
+        else:
+            print("invalid option try again\n") 
             exit
-    exit
+    elif action =="stop":
+        print("game over have a nice day! \n")
+        playing = False
+        exit
