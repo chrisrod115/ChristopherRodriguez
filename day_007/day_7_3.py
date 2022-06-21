242bf3d0d331534d80e6a6c6c1a2dac14d96c020
Christopher Rodriguez <chrisrod115@gmail.com>
2022-06-20 21:08:44 -0700

    day_7_complete
    finally finished all the different programs.

--- day_007/day_7_3.py
+++ day_007/day_7_3.py
@@ -1,5 +1,5 @@
 #Step 3
-
+from replit import clear
 import random
 
 from sympy import false
@@ -16,20 +16,23 @@ for _ in range(word_length):
     display += "_"
 
 #TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-
-guess = input("Guess a letter: ").lower()
-
-#Check guessed letter
-def word_checker():
-    for position in range(word_length):
-        letter = chosen_word[position]
-        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-        if letter == guess:
-            display[position] = letter
-    print(display)
-word_checker()
 playing = True
 count = word_length
-"""
 while playing != False:
-    """
+    guess = input("Guess a letter: ").lower()
+    clear()
+    #Check guessed letter
+    def word_checker():
+        for position in range(word_length):
+            letter = chosen_word[position]
+            # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+            if letter == guess:
+                display[position] = letter
+    word_checker()
+    if "_" in display:
+        print(display)
+        continue
+    else:
+        print(display)
+        print("you win!")
+    
