1237370599c920dc11c4bac5cfa61950ce2d690a
Christopher Rodriguez <chrisrod115@gmail.com>
2022-07-02 22:59:22 -0700

    day_10_calc_app
    Started working on the calculator application.

---
+++ day_010/day_10_CalcApp.py
@@ -0,0 +1,10 @@
+from replit import clear
+from art import logo 
+
+calculating = True
+while calculating != False: 
+    clear()
+    print(logo)
+    user_first_num = input("First number: ")
+    user_second_num = input("Second number: ")
+    operating_symbol = input("Operator type:\n1 for '+'\n2 for '-'\n3 for '*'\n4 for '/'")
