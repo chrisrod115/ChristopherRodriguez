2e3c6de1f3e6533fe756430d05d1411a90b8fe11
Christopher Rodriguez <chrisrod115@gmail.com>
2022-07-06 23:27:34 -0700

    day_11_capstone_v1
    Was invited to a dodgeball game today, and they won!! Other than that I was able to complete the first portion of the state machine.

--- day_011_Capstone/day_11_Blackjack.py
+++ day_011_Capstone/day_11_Blackjack.py
@@ -1,3 +1,28 @@
-    """
+"""
+This program is a BlackJack gambling game. 
+"""
+import random as rnd
+def blackjack():
+    cards = {
+        "user_cards": [],
+        "dealer_cards": [],
+        "deck": [1,2,3,4,5,6,7,8,9,10,11]
+    }
+    shuffle_deck = rnd(cards["deck"])
+    for i in range(2):
+        
     
-    """
+
+playing = True
+while playing != False:
+    wanna_play = input("Do you want to play a blackjack game 'y' or 'n': ")
+    if wanna_play == 'y':
+        blackjack
+    elif wanna_play == 'n':
+        print("Goodbye!")
+        playing = False
+    else:
+        print("Not an option try again")
+        continue
+        
+
