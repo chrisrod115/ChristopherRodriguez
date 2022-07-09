c0cb3d26a5e84f4aa4bf47959b0144438d05a804
Christopher Rodriguez <chrisrod115@gmail.com>
2022-07-08 22:18:22 -0700

    day_11_capstone_v2
    Today I was able to finish most of the capstone project with a very blunt method. This was probably one of the most interesting projects that I have worked on since it was difficult to utilize the things that I am learning. I used the following standard methods including; dictionaries, lists, arrays, functions, flow control and logic diagrams to complete the project. It was probably the most I ever learned in a single sitting.

--- day_011_Capstone/day_11_Blackjack.py
+++ day_011_Capstone/day_11_Blackjack.py
@@ -1,28 +1,69 @@
 """
 This program is a BlackJack gambling game. 
 """
-import random as rnd
-def blackjack():
-    cards = {
-        "user_cards": [],
-        "dealer_cards": [],
-        "deck": [1,2,3,4,5,6,7,8,9,10,11]
-    }
-    shuffle_deck = rnd(cards["deck"])
-    for i in range(2):
-        
+import random
+from replit import clear
+clear()
+cards = {
+    "user_cards": [],
+    "dealer_cards": [],
+    "deck": [1,2,3,4,5,6,7,8,9,10,11]
+}        
+def shuffle_init_cards():
+    num_cards = 2
+    for i in range(num_cards):
+        shuffle_deck = random.choice(cards["deck"])
+        cards["user_cards"].append(shuffle_deck)
+    for i in range(num_cards):
+        dealer_deck = random.choice(cards["deck"])
+        cards["dealer_cards"].append(dealer_deck)
+    print("Your cards are: ", cards["user_cards"])
+    print(f"Dealer is showing: ",cards["dealer_cards"][1])
     
+def hit_or_stay():
+    user_input_hit_or_stay = input("\nType 'hit' or 'stay': ")
+    if user_input_hit_or_stay == "hit":
+        add_one_card = random.choice(cards["deck"])
+        cards["user_cards"].append(add_one_card)
+            
+def winner_or_looser():
+    if sum(cards["user_cards"]) > 21: 
+        print(f"You went over 21 {cards['user_cards']} you loose!")
+    elif sum(cards["dealer_cards"]) > 21: 
+        print(f"Dealer busts {cards['dealer_cards']} you are the winner!")
+    else:
+        if sum(cards["user_cards"]) > sum(cards["dealer_cards"]):
+            print("you are the winner your cards are: ",cards["user_cards"])
+            print("Dealers cards: ", cards["dealer_cards"])
+        elif sum(cards["dealer_cards"]) > sum(cards["user_cards"],):
+            print("sorry you lost! dealer cards are: ", cards["dealer_cards"])
+            print("your cards are:  ", cards["user_cards"])
+        else:
+            print("You pushed, the game is a tie!")
+        
+def dealer_hits_16():
+    add_a_card = random.choice(cards["deck"])
+    cards["dealer_cards"].append(add_a_card)
+    winner_or_looser()
+    
+shuffle_init_cards()
+hit_or_stay()
+while sum(cards["dealer_cards"]) < 16:
+    dealer_hits_16()
+winner_or_looser()
 
+        
+"""
 playing = True
 while playing != False:
     wanna_play = input("Do you want to play a blackjack game 'y' or 'n': ")
     if wanna_play == 'y':
-        blackjack
+        shuffle_cards()
     elif wanna_play == 'n':
         print("Goodbye!")
         playing = False
     else:
         print("Not an option try again")
         continue
-        
+        """
 
