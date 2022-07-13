c036e776124da1eb988422e3b2e69d71e69d94d3
Christopher Rodriguez <chrisrod115@gmail.com>
2022-07-12 22:35:10 -0700

    day_12_v2
    Changed the way that the program flow operated and this was due to the fact that the original program that I had written was not correct and was actually too complicated. Was able to correct it and make it simpler.

--- day_012/day_12_guessGame.py
+++ day_012/day_12_guessGame.py
@@ -1,27 +1,31 @@
 from replit import clear
+from what_are_the_odds import logo
 import random
-import math
 clear()
+lives = 10 
 num_list = list(range(1,101))
 hidden_num = []
+
 def store_num():
     hidden_num.append(random.choice(num_list))
     print(hidden_num)
-def guess_a_num(lives_left):
-    print(hidden_num)
-    guess = input("Make a guess: ")
-    if guess != hidden_num:
-        lives_left -= 1 
-    else:
-        print("congrats you won!!! NO WAY YOU GESSED THAT YOU HAVE SUPERPOWERS")
-        lives_left = 0 
-print("Guess a number from 1 to 100.")
-user_choice = input("Choose 'easy' or 'hard': ")
-if user_choice == "easy":
-    store_num()
-    lives = 10 
-    while lives != 0 or guess_a_num() == hidden_num:
-        guess_a_num(lives_left = lives)
+store_num()
+
+playing = True
+while playing != False:
+    
+    guess = input("Guess a number: ")
+    if lives ==0:
+        playing = False
+    elif guess != hidden_num:
+        lives -= 1
+    elif guess == hidden_num: 
+        print("OMG you found the hidden number!!!")
+        playing = False
+    print(f"Lives left: {lives}")
+    
+    
+        
         
         
     
