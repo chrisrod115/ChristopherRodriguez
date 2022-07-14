a6600ab7c95ad30e727bd5ea9b1e361e958788db
Christopher Rodriguez <chrisrod115@gmail.com>
2022-07-13 21:26:49 -0700

    day_12_complete
    Finished the program about what are the odds. The premise of this program is to take a random number and try to guess that hidden number. At the beginning of the game the user is given an option to choose one of two paths. Either the 'easy' path or the 'hard' path. This determines how many lives the user will begin with.

--- day_012/day_12_guessGame.py
+++ day_012/day_12_guessGame.py
@@ -5,24 +5,37 @@ clear()
 lives = 10 
 num_list = list(range(1,101))
 hidden_num = []
-
+game_mode = {
+    "easy": 10,
+    "hard": 5,
+}
 def store_num():
     hidden_num.append(random.choice(num_list))
-    print(hidden_num)
 store_num()
 
+user_input_mode = str(input("Pick a mode to play: 'easy' or 'hard': "))
+if user_input_mode == "hard":
+    lives = game_mode["hard"]
+else:
+    lives == game_mode["easy"]
+
+    
 playing = True
 while playing != False:
+        print(logo)
+        guess = int(input("Guess a number: "))
+        if lives ==0:
+            print(f"Lives left: {lives}")
+            playing = False
+        elif guess == hidden_num[0]: 
+            print("OMG you found the hidden number!!!")
+            playing = False        
+        elif guess != hidden_num:
+            clear()
+            print(f"Lives left: {lives}")
+            lives -= 1
     
-    guess = input("Guess a number: ")
-    if lives ==0:
-        playing = False
-    elif guess != hidden_num:
-        lives -= 1
-    elif guess == hidden_num: 
-        print("OMG you found the hidden number!!!")
-        playing = False
-    print(f"Lives left: {lives}")
+    
     
     
         
